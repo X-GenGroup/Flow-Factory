e171116605d8a27f1dd3e10e1be905552d896018
Jayce-Ping <315229706@qq.com>
2026-01-22 18:08:32 +0800

    Fix version_utils

--- src/flow_factory/utils/version.py
+++ src/flow_factory/utils/version.py
@@ -1,3 +1,20 @@
+# Copyright 2026 Jayce-Ping
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# src/flow_factory/utils/version.py
+"""
+Version comparison utilities.
+"""
 from packaging import version
 import importlib.metadata
 
@@ -5,11 +22,14 @@ def compare_lib_version(lib_name: str, target_version: str) -> int:
     """
     Compare the version of given lib and target version
     
-    返回结果:
-     1 : installed version  > target version
-     0 : installed version == target version
-    -1 : installed version  < target version
-    None: Not installed
+    Args:
+        lib_name: Name of the library to check
+        target_version: Target version string to compare against
+    Returns:
+        1 : installed version  > target version
+        0 : installed version == target version
+        -1 : installed version  < target version
+        None: Not installed
     """
     try:
         installed_ver = importlib.metadata.version(lib_name)
